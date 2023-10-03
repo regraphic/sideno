10b61f023d338733e59bbf2179404ec2978b0b00
Rajdeep Malakar <rajdeepm.dev@gmail.com>
2023-10-03 19:08:49 +0000

    RELEASE 2.1.1

---
+++ pkg/si_img_bg.wasm.d.ts
@@ -0,0 +1,26 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function __wbg_sifont_free(a: number): void;
+export function sifont_from_vec(a: number, b: number): number;
+export function sifont_from_network_async(a: number, b: number): number;
+export function sifont_from_network(a: number, b: number): void;
+export function __wbg_siimage_free(a: number): void;
+export function siimage_from_vec(a: number, b: number, c: number): number;
+export function siimage_from_network_async(a: number, b: number, c: number): number;
+export function siimage_from_network(a: number, b: number, c: number): void;
+export function siimage_text(a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number): number;
+export function siimage_to_bytes(a: number, b: number): void;
+export function siimage_set_font(a: number, b: number): number;
+export function siimage_height(a: number): number;
+export function siimage_width(a: number): number;
+export function sifont_new(a: number, b: number): number;
+export function siimage_new(a: number, b: number, c: number): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h72bc8f83b7892bd9(a: number, b: number, c: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function wasm_bindgen__convert__closures__invoke2_mut__h07a46436fd1a5ba1(a: number, b: number, c: number, d: number): void;
