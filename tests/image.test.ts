ac7de180254efbb73374ad969a192e667280e078
Rajdeep Malakar <rajdeepm.dev@gmail.com>
2023-10-31 23:59:50 +0530

    Update image.test.ts

--- tests/image.test.ts
+++ tests/image.test.ts
@@ -3,6 +3,7 @@ import { Image, Font } from "../mod.ts";
 let font = new Font("https://github.com/Zype-Z/ShareImage.js/raw/main/assets/fonts/sirin-stencil.ttf");
 let img = new Image("https://res.cloudinary.com/zype/image/upload/regraphic");
 await img.init();
+await font.init()
 
 await img.text("Hello, World!", 64, 480, 254, font);
 await img.text("Hello, Tagline!", 64, 480, 300, font);
