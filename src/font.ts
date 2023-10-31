2afff6c166e173eb04ad7628c6d8b2501086c9b2
Rajdeep Malakar <rajdeepm.dev@gmail.com>
2023-10-31 14:25:42 +0530

    Refractor font

--- src/font.ts
+++ src/font.ts
@@ -1,13 +1,19 @@
 import { SiFont } from "../deps.ts";
 
 export class Font {
-    public font: Promise<SiFont>
+    #font: Promise<SiFont>;
+    public font?: SiFont;
     constructor(src: string | Uint8Array) {
         if (typeof src === "string") {
-            this.font = new Promise<SiFont>(async (resolve) => resolve(await SiFont.from_network_async(src)));
+            this.#font = new Promise<SiFont>(async resolve => resolve(await SiFont.from_network_async(src)));
         } else {
-            this.font = new Promise<SiFont>(async (resolve) => resolve(new SiFont(src)))
+            this.#font = new Promise<SiFont>(async resolve => resolve(await SiFont.from_vec(src)))
         }
+    }
+
+    async init(): Font {
+        this.font = await this.#font;
+        return this;
     }
 }
 
