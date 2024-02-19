b8fde48313af9b07c0282aee7410a393737e536f
Rajdeep Malakar <rajdeepm.dev@gmail.com>
2024-02-20 01:49:30 +0530

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -3,7 +3,7 @@ import * as core from "./pkg/si_img.js";
 // @deno-types="./pkg/si_img.d.ts"
 export * from "./pkg/si_img.js";
 
-export {encode as encodeB64} from "@std/encoding/base64";
+export {encodeBase64 as encodeB64} from "@std/encoding/base64";
 
 export { core };
 export default core;
